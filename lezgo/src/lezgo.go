98b9f6954b5309ccf9f21021d1d8419bd8af3c5e
Rafael CATTIN <rafael.cattin@etu.imt-nord-europe.fr>
2022-02-24 15:41:48 +0100

    path recovery done

--- lezgo/src/lezgo.go
+++ lezgo/src/lezgo.go
@@ -1,14 +1,35 @@
 package main
 
 import (
-
+	"path/filepath"
+	"fmt"
+	"flag"
 )
 
 func main() {
-	initNode := &node{ path: ""}
+	dFlag := parseFlags()
+
+	initPath, err := filepath.Abs(dFlag)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	initNode := &node{ path: initPath}
 	search := &search{}
 	size := &size{ size : 0}
 
+	fmt.Println(initNode.path)
+
 	initNode.accept(search)
 	initNode.accept(size)
+}
+
+func parseFlags() string {
+	var d string
+
+        flag.StringVar(&d, "d", ".", "The filepath where the search begins")
+
+	flag.Parse()
+	return d
 }
